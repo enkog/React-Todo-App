163ce5fa62dce70fdca970d73e44ccef38c2d8ec
enkog <nkirukaogu@gmail.com>
2021-09-20 12:21:33 +0100

    Add onChange event to the checkboxes

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,30 +3,35 @@ import TodosList from './TodosList';
 import Header from './Header';
 
 class TodoContainer extends React.Component {
-    state = {
-        todos: [
-          {
-            id: 1,
-            title: "Setup development environment",
-            completed: true
-          },
-          {
-            id: 2,
-            title: "Develop website and add content",
-            completed: false
-          },
-          {
-            id: 3,
-            title: "Deploy to live server",
-            completed: false
-          }
-        ]
-       };
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+  };
+
+  handleChange = (id) => {
+    console.log("clicked", id);
+  };
+
   render() {
     return(
       <div>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} />
       </div>
     );
   }
