41bc653af7d4e51ec8348e0fbf8425df41044f47
enkog <nkirukaogu@gmail.com>
2021-09-20 11:26:29 +0100

    Add header in the Header component

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TodosList from './TodosList';
+import Header from './Header';
 
 class TodoContainer extends React.Component {
     state = {
@@ -23,7 +24,10 @@ class TodoContainer extends React.Component {
        };
   render() {
     return(
+      <div>
+        <Header />
         <TodosList todos={this.state.todos} />
+      </div>
     );
   }
 }
